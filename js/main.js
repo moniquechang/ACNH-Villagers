4eecdbfed76d8be1a0aa96ef443694bc52444f69
Monique Chang <moniquechang1997@gmail.com>
2022-08-03 02:33:39 +0000

    #2 added event handler function to hide all villagers except the one with the name user submits in searchbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var dataArr = [];
 var $ul = document.querySelector('ul');
+var searchInput = document.querySelector('input');
+var $form = document.querySelector('form');
+// var searchDefaultText = document.querySelector('h2');
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://acnhapi.com/v1/villagers/');
@@ -17,6 +20,7 @@ xhr.send();
 function renderVillager(data) {
   var $li = document.createElement('li');
   $li.setAttribute('class', 'column-full column-fourth');
+  $li.setAttribute('data-villager', data.name['name-USen']);
 
   var $div = document.createElement('div');
   $div.setAttribute('class', 'villager-polaroid');
@@ -34,3 +38,19 @@ function renderVillager(data) {
 
   return $li;
 }
+
+function handleSearchInputSubmit(event) {
+  event.preventDefault();
+  var liNodeList = document.querySelectorAll('li');
+  for (var i = 0; i < liNodeList.length; i++) {
+    liNodeList[i].className = 'column-full column-fourth hidden';
+  }
+  for (var k = 0; k < liNodeList.length; k++) {
+    if (searchInput.value === liNodeList[k].getAttribute('data-villager')) {
+      liNodeList[k].className = 'column-full column-fourth';
+      break;
+    }
+  }
+}
+
+$form.addEventListener('submit', handleSearchInputSubmit);
